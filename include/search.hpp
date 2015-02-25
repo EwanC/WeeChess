f494e9b1bae7e5d818b1d4c37753cce9c6b2d34c
EwanC <ewan.cr@gmail.com>
2015-02-25 22:09:53 +0000

    Start of search

---
+++ include/search.hpp
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "board.hpp"
+
+bool isRepetition(const Board& b);
+
+
+
+
+#endif //SEARCH_H
---
+++ source/search.cpp
@@ -0,0 +1,15 @@
+#include "search.hpp"
+#include <cassert>
+
+bool isRepetition(const Board& b) 
+{
+
+	for(int index = b.m_hisply - b.m_fiftyMove; index < b.m_hisply-1; ++index) {	
+		assert(index >= 0 && index < MAX_GAME_MOVES);		
+		if(b.m_posHash == b.m_history[index].posHash) {
+			return true;
+		}
+	}	
+
+	return false;
+}
